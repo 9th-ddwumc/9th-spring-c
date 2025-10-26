3b7a6fcd874178971c780f354f4ae08c5e2eac9d
이서연 <redchk35@naver.com>
2025-10-26 20:09:10 +0900

    불필요한 파일 정리

--- src/main/java/com/example/umc9th2/domain/member/enums/Address.java
+++
@@ -1,5 +0,0 @@
-package com.example.umc9th2.domain.member.enums;
-
-public enum Address {
-    강남구
-}
--- src/main/java/com/example/umc9th2/domain/member/enums/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.example.umc9th2.domain.member.enums;
-
-public enum Gender {
-    MALE, FEMALE, NONE
-}
