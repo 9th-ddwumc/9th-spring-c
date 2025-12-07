c3203ba7786b90c2a1f6e5d147ff9e5369b90a07
이서연 <redchk35@naver.com>
2025-12-07 10:01:16 +0900

    파일 수정

--- auth/enums/CustomUserDetails.java
+++ auth/enums/CustomUserDetails.java
@@ -28,23 +28,24 @@ public class CustomUserDetails implements UserDetails {
         return member.getEmail();
     }
 
+    /* 추가해야 하는 UserDetails 기본 메서드들 */
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return true; // 계정 만료 여부 (false면 로그인 불가)
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return true; // 계정 잠김 여부
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return true; // 비밀번호 만료 여부
     }
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return true; // 계정 활성화 여부
     }
 }
